da35bff93dac5b8f1ffb492ee0244b9e61650e9f
Santiago Vasquez <santivaz852@hotmail.com>
2024-07-12 21:51:12 -0500

    Creating footer to receive Store information

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1223708625"
+    serviceWorkerVersion: "2000838756"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,14 +38,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ac52a55135ac36b84616b653a074528f
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "a529d1369223786245a8f27a8db9eb7b",
+"flutter_bootstrap.js": "a2110ccc9ca44c252aee8b7445df8418",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9bfd73fee42e7061abedf58d9ddcb2ce",
-"/": "9bfd73fee42e7061abedf58d9ddcb2ce",
-"main.dart.js": "b119a638017ea1f33174a2429f37db71",
+"index.html": "8102d6381e5898394114fbd346630c49",
+"/": "8102d6381e5898394114fbd346630c49",
+"main.dart.js": "a4053421e990a7d3235edebcb9e45d23",
 "manifest.json": "a782efd34a7f4232322c4e252eaecbd0",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
