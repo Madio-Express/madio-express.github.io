85abdbecc2701a004989d157f675c5f1d133b8e7
Santiago Vasquez <santivaz852@hotmail.com>
2024-07-12 20:45:48 -0500

    Make responsive design for the show product Screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "45754682"
+    serviceWorkerVersion: "1223708625"
   }
 });
