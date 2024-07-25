8f60f87c872321e898980c20240bcc6cd328b4ac
Santiago Vasquez <santivaz852@hotmail.com>
2024-07-24 19:41:18 -0500

    Fixing categories and subcategories lists and banner buttons opens links twice

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,14 +38,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ac52a55135ac36b84616b653a074528f
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "5b5230f98f8cdae5f63a56c93a8a32f5",
+"flutter_bootstrap.js": "8b5b1d5c1008c7d2360511e11944b9da",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "fc43c2281f7cf41dd51ecf9838c6b9f2",
-"/": "fc43c2281f7cf41dd51ecf9838c6b9f2",
-"main.dart.js": "a3d20c4f758b42d137d6b666513c4728",
+"index.html": "2010f363c6ab51bb88ea7237b02894e3",
+"/": "2010f363c6ab51bb88ea7237b02894e3",
+"main.dart.js": "21199248408e70068d7d57a6d9e89dbb",
 "manifest.json": "a782efd34a7f4232322c4e252eaecbd0",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
