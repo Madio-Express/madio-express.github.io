8c4346120a709ae0fcaad3a626022529c526cc56
Santiago Vasquez <santivaz852@hotmail.com>
2024-07-24 20:20:03 -0500

    Banner buttons and logo desgin improvement

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2084811429"
+    serviceWorkerVersion: "3679735651"
   }
 });
