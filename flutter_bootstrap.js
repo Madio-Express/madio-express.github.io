f5c3104e25d7679e8a2e12c004a717bccd9a1afa
Santiago Vasquez <santivaz852@hotmail.com>
2025-06-21 14:42:28 -0500

    New styles and Primary colors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3294342980"
+    serviceWorkerVersion: "2273912969"
   }
 });
