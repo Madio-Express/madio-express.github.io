3d602aa278f4399d2d78cf822ecf278a3086bf34
Santiago Vasquez <santivaz852@hotmail.com>
2025-06-20 21:23:15 -0500

    Solving firebase problem

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,14 +42,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3cd674ad1f06416f09fd3249b9f9d444
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "cc787277d02f0457cfbd64eff4013645",
+"flutter_bootstrap.js": "973b8238c97bb23aabdbae70d24ed741",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "df59dff671b3982507f16bf69f52a45e",
-"/": "df59dff671b3982507f16bf69f52a45e",
-"main.dart.js": "af9e9d461856749a8bf7d7f24a78523b",
+"index.html": "ada2030d68276a8a700c506b1d6aaa31",
+"/": "ada2030d68276a8a700c506b1d6aaa31",
+"main.dart.js": "90c1831e6529714443c9b685f517e914",
 "manifest.json": "a782efd34a7f4232322c4e252eaecbd0",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
