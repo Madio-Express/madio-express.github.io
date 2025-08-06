d382f909014c89460467dfac6c1193e38bb5162f
Santiago Vasquez <santivaz852@hotmail.com>
2025-08-05 21:06:21 -0500

    order tracking screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1379436131"
+    serviceWorkerVersion: "732351823"
   }
 });
