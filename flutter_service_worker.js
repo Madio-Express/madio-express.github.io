f2b64371a67f75a75b4b81fafdf04f41944fda6b
Santiago Vasquez <santivaz852@hotmail.com>
2025-08-29 17:13:02 -0500

    Fixing Delivery price

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3f9173f3a76d13c172da3abb53ada80
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "a2147bf911b5147d5441b6d216bf3b2b",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "b21efa0f89eb847a44c9294a4afe0fc2",
+"flutter_bootstrap.js": "0bfac0aa52335f60c8cfbf00cae1e799",
 "icons/Icon-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-512.png": "e758359d6ea8360d6aa518280604a75c",
 "icons/Icon-maskable-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-maskable-512.png": "e758359d6ea8360d6aa518280604a75c",
-"index.html": "f5de3dd8bacfaa906b08a5476be59c41",
-"/": "f5de3dd8bacfaa906b08a5476be59c41",
-"main.dart.js": "19d01729beddee946b2b6981cf6f617f",
+"index.html": "8b39ef105b5a83bbc275d895ec4811a3",
+"/": "8b39ef105b5a83bbc275d895ec4811a3",
+"main.dart.js": "0cdeedd35c37fb1b3d35919440f87852",
 "manifest.json": "42236d0105f957e75c6b0c9acc017965",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
