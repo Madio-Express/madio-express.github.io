2bc36e48d5583a2af4241354f45dcd9477c5c9a2
Santiago Vasquez <santivaz852@hotmail.com>
2025-08-29 20:29:05 -0500

    Improving visual defects

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3f9173f3a76d13c172da3abb53ada80
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "a2147bf911b5147d5441b6d216bf3b2b",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "8d12e1d8b22a010643d0aa57f6b4f9c6",
+"flutter_bootstrap.js": "beb620398291329ef647085ead4fd23d",
 "icons/Icon-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-512.png": "e758359d6ea8360d6aa518280604a75c",
 "icons/Icon-maskable-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-maskable-512.png": "e758359d6ea8360d6aa518280604a75c",
-"index.html": "529ade7619bea34e5bf800ffba616915",
-"/": "529ade7619bea34e5bf800ffba616915",
-"main.dart.js": "b470fea96a1b625f65f6770f1f107f50",
+"index.html": "daf78adec1158bc7591099b83d3f03cb",
+"/": "daf78adec1158bc7591099b83d3f03cb",
+"main.dart.js": "cde726ba8b1afab5c9516d488dc22fd8",
 "manifest.json": "42236d0105f957e75c6b0c9acc017965",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
