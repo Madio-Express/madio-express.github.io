4ad1dab203f68c54f3dda63bf0c89d5035cc4cb7
Santiago Vasquez <santivaz852@hotmail.com>
2025-09-14 20:39:02 -0500

    Adding store name to the url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,14 +46,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e3f9173f3a76d13c172da3abb53ada80
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "a2147bf911b5147d5441b6d216bf3b2b",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "2e306a26d7cba08ead4255616a081fb0",
+"flutter_bootstrap.js": "acf6b41f076a4e23c47020176091ab17",
 "icons/Icon-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-512.png": "e758359d6ea8360d6aa518280604a75c",
 "icons/Icon-maskable-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-maskable-512.png": "e758359d6ea8360d6aa518280604a75c",
-"index.html": "eb15c9af501709f429c49080c165059b",
-"/": "eb15c9af501709f429c49080c165059b",
-"main.dart.js": "11ce3c3a8530c7a7d6a186a4b1d274d5",
+"index.html": "911b2722c25109d13a8837863ed15c8e",
+"/": "911b2722c25109d13a8837863ed15c8e",
+"main.dart.js": "8ba1679b8b266ffa3f88bf4f1ee38430",
 "manifest.json": "42236d0105f957e75c6b0c9acc017965",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
