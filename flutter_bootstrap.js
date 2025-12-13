488e12ec81656fe536cd994bbda65bed897eeb34
Santiago Vasquez <santivaz852@hotmail.com>
2025-12-12 20:52:23 -0500

    Single Product page. Fully functional

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2570667814"
+    serviceWorkerVersion: "2990522204"
   }
 });
