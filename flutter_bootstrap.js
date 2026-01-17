4dd892ae343c82177a3dc9bcded85ee506fb0409
Santiago Vasquez <santivaz852@hotmail.com>
2026-01-17 12:48:28 -0500

    Claude new checkout screen design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "170971927"
+    serviceWorkerVersion: "1779616851"
   }
 });
