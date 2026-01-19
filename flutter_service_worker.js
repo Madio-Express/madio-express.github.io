9a246e219d891aacb3e402495b663dac1997ac7d
Santiago Vasquez <santivaz852@hotmail.com>
2026-01-18 19:52:45 -0500

    Improving Categories section with subcategories

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,14 +47,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c6bde94130829683f66d304eb60ffe4d
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "a2147bf911b5147d5441b6d216bf3b2b",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f609a0ab167a6fcf9c7a960739e8791e",
+"flutter_bootstrap.js": "8739faa1048718c1999695cc07d904ec",
 "icons/Icon-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-512.png": "e758359d6ea8360d6aa518280604a75c",
 "icons/Icon-maskable-192.png": "d221d6ae1e2db704afb970a818399b7e",
 "icons/Icon-maskable-512.png": "e758359d6ea8360d6aa518280604a75c",
-"index.html": "ce6af992ef4b839f675bffe408901407",
-"/": "ce6af992ef4b839f675bffe408901407",
-"main.dart.js": "3cdf72c0cb5f83920de30caa664cd8eb",
+"index.html": "8ffa9cb17b433305a279430f52ccc71b",
+"/": "8ffa9cb17b433305a279430f52ccc71b",
+"main.dart.js": "856f321c740e4f3fa9823ff0312333fa",
 "manifest.json": "42236d0105f957e75c6b0c9acc017965",
 "version.json": "ef54a4ae329e507823db7566997fe9c3"};
 // The application shell files that are downloaded before a service worker can
