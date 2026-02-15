d80fcb297eee57b41c615c345de42e3db56bcfa1
Santiago Vasquez <santivaz852@hotmail.com>
2026-02-15 16:16:26 -0500

    Fixing discounts display for products

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3324309168"
+    serviceWorkerVersion: "987908025"
   }
 });
