79bd0cf03d243abe17a5a650b03ade7fc57b6ea3
Santiago Vasquez <santivaz852@hotmail.com>
2026-02-25 13:57:51 -0500

    OrderTrackingScreen responsive design 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "729178669"
+    serviceWorkerVersion: "3297480346"
   }
 });
