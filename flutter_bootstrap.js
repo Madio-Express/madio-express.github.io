8494d8c813b109131caf43795a5d1a3673c0c3db
Santiago Vasquez <santivaz852@hotmail.com>
2026-05-05 20:07:53 -0500

    Improving product detail screen and text colors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2281963408"
+    serviceWorkerVersion: "918221382"
   }
 });
