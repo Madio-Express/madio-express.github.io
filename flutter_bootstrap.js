4be7950d4750c6e09f50c1b1894c63d89837f6f0
Santiago Vasquez <santivaz852@hotmail.com>
2026-05-22 13:24:45 -0500

    google sign in web button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4250526291"
+    serviceWorkerVersion: "2400976565"
   }
 });
