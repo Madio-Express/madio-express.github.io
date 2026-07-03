d5896462fbc544ebef4f6669d5482faefb70c7c5
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-03 15:56:09 -0500

    deploy: politica de privacidad estatica

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3428152939"
+    serviceWorkerVersion: "3094995988"
   }
 });
