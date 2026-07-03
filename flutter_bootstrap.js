4a342acf729ce3f3157504d35e68d0220bd007c0
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-02 19:12:22 -0500

    deploy: fix checker de version (validar forma del build id)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "374010144"
+    serviceWorkerVersion: "3457939576"
   }
 });
