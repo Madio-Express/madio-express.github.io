837832962a6d111f54d9444b2c6b5d534d90dca4
Santiago Vasquez <santivaz852@hotmail.com>
2026-07-03 14:53:22 -0500

    deploy: creativo social + scroll s=amazon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "417395582"
+    serviceWorkerVersion: "3428152939"
   }
 });
